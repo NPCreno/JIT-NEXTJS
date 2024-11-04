d09fc0976894f735ec49aaf610ec411bd96299b1
NPCreno <162246858+lorlorenss@users.noreply.github.com>
2024-11-04 16:47:49 +0800

    alignments in people List

--- src/app/peopleList/page.tsx
+++ src/app/peopleList/page.tsx
@@ -44,11 +44,11 @@ export default function PeopleList() {
                     <table className="w-full">
                         <tbody>
                             {people.map((person, index) => (
-                                <tr key={index} className="group cursor-pointer hover:bg-gray-50">
+                                <tr key={index} className="group cursor-pointer rounded-lg hover:bg-gray-100">
                                     <td className=" px-6 py-4 text-gray-700 max-w-10 max-h-10">
                                         <img src={person.photo} className="max-w-16 max-h-16 rounded-full" alt="" />
                                     </td>
-                                    <td>
+                                    <td className="flex flex-col gap-4 justify-center h-full h-24">
                                         <div className="font-bold text-lg text-gray-700">
                                             {person.name}
                                         </div>
